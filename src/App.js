0f5224ea4c5169491a31155b1179c63838c27f45
Jackson Tam <Jackson.Tam@OOCL.com>
2024-12-07 18:28:25 +0800

    feat: update APP.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
+import React from 'react';
 import './App.css';
-import ParkingLot from "./components/ParkingLot";
-function App() {
+import ParkingLot from './components/ParkingLot';
+import { ParkingLotProvider } from './components/ParkingLotContext';
+
+const App = () => {
   return (
-      <div>
-        <ParkingLot/>
-      </div>
+      <ParkingLotProvider>
+        <div>
+          <ParkingLot />
+        </div>
+      </ParkingLotProvider>
   );
-}
+};
+
 export default App;
